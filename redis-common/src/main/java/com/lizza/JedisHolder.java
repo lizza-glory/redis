3b07cfd38bfe528b44c5aa7d8adf87d48d46fa88
liutao1 <liutao1@mokahr.com>
2022-07-28 15:56:41 +0800

    commit

--- redis-common/src/main/java/com/lizza/JedisHolder.java
+++ redis-common/src/main/java/com/lizza/JedisHolder.java
@@ -10,7 +10,12 @@ import redis.clients.jedis.Jedis;
 public class JedisHolder {
 
     public static Jedis jedis() {
-        Jedis jedis = new Jedis("192.168.151.140", 6379);
+        Jedis jedis = new Jedis("localhost", 6379);
+        jedis.auth("root");
         return jedis;
+    }
+
+    public static void main(String[] args) {
+
     }
 }
